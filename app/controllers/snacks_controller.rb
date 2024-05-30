0f93ca2e1f707341203de0e2191bc6569cbc3f71
ivan chu <ivan.csq@hotmail.com>
2024-05-30 16:46:31 +0800

    form working well

--- app/controllers/snacks_controller.rb
+++ app/controllers/snacks_controller.rb
@@ -1,2 +1,24 @@
 class SnacksController < ApplicationController
+def show
+
+end
+
+  def edit
+    # @user = User.find(params[:id])
+    # @user = current_user
+    @snack = Snack.find(params[:id])
+    # @snack.user = @user
+  end
+
+  def update
+    @snack = Snack.find(params[:id])
+    @snack.update(snack_params)
+    redirect_to snack_path(@snack)
+  end
+
+  private
+
+  def snack_params
+    params.require(:snack).permit(:name, :description, :price, :quantity, :image_url)
+  end
 end

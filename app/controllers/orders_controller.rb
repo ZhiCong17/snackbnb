aada94772fc479bee7ca3b838e4967dcf49d7945
weiyang <weiyang_11@outlook.com>
2024-06-01 15:16:29 +0800

    checkout page

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,13 @@
 class OrdersController < ApplicationController
-
   def show
     @user = current_user
-    @order = Order.find_by(user_id: @user.id)
-    @order_list = OrderItem.where(order_id: @order.id)
+    @order_lists = []
+    @total = 0
+    @order = Order.where(user_id: @user.id)
+    @order.each do |order|
+      order
+      @order_list = OrderItem.where(order_id: order.id)
+      @order_lists << @order_list
+    end
   end
 end
